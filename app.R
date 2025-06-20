fc0ad4f1ce5b827ff7f264dab2d480fae6ff1481
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2025-06-20 14:13:07 -0700

    wip: greeting by state

--- app.R
+++ app.R
@@ -9,22 +9,27 @@ N_SECTORS <- 35 * 2
 MIPS_BR <- read_rds("data/mips_br.Rds")
 STATES <- names(MIPS_BR)
 
-current_state <- get_ZALfx_multipliers(
-  MIPS_BR[["morelia"]],
-  N_SECTORS
-)
-
-simulate_demand_shocks(rep(2.0, N_SECTORS),
-  current_state$L,
-  current_state$f,
-  current_state$x,
-  shocks_are_multipliers = TRUE
-)
+# current_state <- get_ZALfx_multipliers(
+#   MIPS_BR[["morelia"]],
+#   N_SECTORS
+# )
+#
+# simulate_demand_shocks(rep(2.0, N_SECTORS),
+#   current_state$L,
+#   current_state$f,
+#   current_state$x,
+#   shocks_are_multipliers = TRUE
+# )
 
 # ---- app
 
-ui <- fluidPage()
+ui <- fluidPage(
+  selectInput("state", "Choose state to simulate:", STATES),
+  textOutput("greeting")
+)
 
-server <- function(input, output, session) {}
+server <- function(input, output, session) {
+  output$greeting <- renderText(input$state)
+}
 
 shinyApp(ui, server)
