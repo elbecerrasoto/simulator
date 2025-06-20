92405b7880906af5c6aaf0570ae8a990451e97d8
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2025-06-20 12:04:46 -0700

    from scratch

--- app.R
+++ app.R
@@ -1,7 +1,5 @@
-# Carga librerías y recursos
-source("global.R", local = TRUE) # Opcional para código compartido
+source("global.R", local = TRUE)
 
-# Ejecuta la app
 shinyApp(
   ui = source("ui.R", local = TRUE)$value,
   server = source("server.R", local = TRUE)$value
--- global.R
+++
@@ -1,9 +0,0 @@
-library(shiny)
-library(shinydashboard)
-library(DT)
-library(readr)
-
-# Carga datos compartidos
-states <- read.csv("data/states.csv", encoding = "UTF-8")
-multi <- read.delim("data/multiplier_cdmx.tsv", sep = "\t")
-mipsBR <- read_rds("data/mipsBR.Rds")
