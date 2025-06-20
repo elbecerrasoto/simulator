a54d69b487fab80b955f6ec624a3e4eb8cd9a410
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2025-06-20 15:01:48 -0700

    set up simulator

--- app.R
+++ app.R
@@ -25,14 +25,33 @@ STATES <- names(MIPS_BR)
 
 ui <- fluidPage(
   selectInput("state", "Choose state to simulate:", STATES),
-  dataTableOutput("dynamic"),
-  textOutput("debug")
+  sliderInput("shock_multiplier", "Increase on final demand by sector.\n(Multiply by this amount.)",
+    value = 1.01, min = 0.50, max = 2.00
+  ),
+  dataTableOutput("multipliers")
 )
 
 server <- function(input, output, session) {
   Z_aug <- reactive(MIPS_BR[[input$state]])
   ZALfx_multipliers <- reactive(get_ZALfx_multipliers(Z_aug(), N_SECTORS))
-  output$dynamic <- renderDataTable(ZALfx_multipliers()$multipliers)
+
+  L <- reactive(ZALfx_multipliers()$L)
+  f <- reactive(ZALfx_multipliers()$f)
+  x <- reactive(ZALfx_multipliers()$x)
+  multipliers <- reactive(ZALfx_multipliers()$multipliers)
+
+  output$multipliers <- renderDataTable(multipliers())
+
+  shock_multipliers <- reactive(rep(input$shock_multiplier, N_SECTORS))
+  results <- reactive(
+    simultate_demand_shocks(
+      shock_multipliers(),
+      L(),
+      f(),
+      x(),
+      shocks_are_mulipliers = TRUE
+    )
+  )
 }
 
 shinyApp(ui, server)
